b41e6173529711506c7e2e49a72e5eccf6ad2a4f
Minh Nguyen <nqminhuit@gmail.com>
2021-03-26 00:01:29 +0700

    - add grid

--- src/view/GiphyGallery.jsx
+++ src/view/GiphyGallery.jsx
@@ -7,26 +7,21 @@ export default function GiphyGallery() {
     <>
       <h1 className="text-center">Wellcome to Giphy Trending!</h1>
       <div className="container bg-light d-flex flex-wrap justify-content-evenly">
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
-        <GifCard />
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
       </div>
     </>
   );
