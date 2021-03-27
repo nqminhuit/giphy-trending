e76aa408d038a7930a9936545b681d0d728b8969
Minh Nguyen <nqminhuit@gmail.com>
2021-03-27 18:20:23 +0700

    - add infinite scroll to fetch more gifs

--- src/view/GiphyGallery.jsx
+++ src/view/GiphyGallery.jsx
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from "react";
 import GifCard from "../components/GifCard.jsx";
-import AppConstants from "../constants/AppConstants.js";
+import Constants from "../constants/AppConstants.js";
 import { fetchGifs } from "../controllers/FetchGifs.js";
 import { randomNumber } from "../utils/RandomNumber.js";
 
 export default function GiphyGallery() {
   const [gifs, setGifs] = useState([]);
+  const [offset, setOffset] = useState(0);
 
   useEffect(() => {
-    fetchGifs(AppConstants.GIPHY_TRENDING_ENDPOINT, AppConstants.API_KEY, 0, 20)
-      .then(setGifs)
+    window.addEventListener("scroll", fetchMoreGifs, true);
+    return function cleanUpScrollEvent() {
+      console.log("mmm remove on scroll");
+      window.removeEventListener("scroll", fetchMoreGifs);
+    };
+  });
+
+  useEffect(() => {
+    fetchGifs(Constants.GIPHY_TRENDING_ENDPOINT, Constants.API_KEY, offset, Constants.LIMIT_GIFS_PER_LOAD)
+      .then(newGifs => setGifs(oldGifs => oldGifs.concat(newGifs)))
       .catch(console.error);
-  }, []);
+  }, [offset]);
+
+  function fetchMoreGifs() {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 10) { // check total
+      setTimeout(() => setOffset(offset + Constants.LIMIT_GIFS_PER_LOAD), 1000);
+    }
+  };
 
   return (
     <div className="container-fluid">
