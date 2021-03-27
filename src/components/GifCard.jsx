2cccc4cc49f513cf1dd26cc2be098ee501e9524e
Minh Nguyen <nqminhuit@gmail.com>
2021-03-27 08:34:16 +0700

    - add bootstrap-icons - add layout for GifCard

--- src/components/GifCard.jsx
+++ src/components/GifCard.jsx
@@ -3,7 +3,23 @@ import React from "react";
 export default function GifCard() {
 
   return (
-    <div className="w-200 h-200 bg-white shadow rounded m-1"></div>
+    <div className="w-75 m-auto">
+      <div className="h-200p bg-white shadow">
+        <img className="img-fluid p-2"
+          src="https://media3.giphy.com/media/r3Q4NsodpaSpwsOzmF/200_s.gif?cid=1405890bx3dbf14mp804pe78sqmpxxggyqz6p5pmz9yfw0t8&rid=200_s.gif"></img>
+        <div>
+          <i className="bi bi-paperclip"></i>
+          <i className="bi bi-eye-fill"></i>
+          <i className="bi bi-chat-quote-fill"></i>
+          <i className="bi bi-heart-fill"></i>
+        </div>
+      </div>
+      <div className="my-3">
+        <a href="#" className="text-decoration-none fw-bold cl-royalblue">
+          username
+        </a>
+      </div>
+    </div>
   );
 
 }
--- src/view/GiphyGallery.jsx
+++ src/view/GiphyGallery.jsx
@@ -7,25 +7,25 @@ export default function GiphyGallery() {
     <>
       <h1 className="text-center">Wellcome to Giphy Trending!</h1>
       <div className="container bg-light d-flex flex-wrap justify-content-evenly">
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
-        <div className="col-6 col-md-4 col-lg-3"><GifCard /></div>
+        {
+          gifCards().map(i => (
+            <div key={i} className="col-6 col-md-4 col-lg-3 my-3">
+              <GifCard />
+            </div>
+          ))
+        }
       </div>
     </>
   );
 
+}
+
+function gifCards() {
+  const cards = [];
+  for (let i = 0; i < 20; i++) {
+    cards.push(i);
+  }
+  return cards;
 }
 
 GiphyGallery.propTypes = {
