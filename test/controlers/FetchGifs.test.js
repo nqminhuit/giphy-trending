1eb0e57da4e0e91cb6fe8c6d6a2218608122af8f
Minh Nguyen <nqminhuit@gmail.com>
2021-03-29 20:25:47 +0700

    - enhance unit tests

--- test/controlers/FetchGifs.test.js
+++ test/controlers/FetchGifs.test.js
@@ -1,16 +1,27 @@
 import axios from "axios";
+import Constants from "../../src/constants/AppConstants.js";
 import { fetchGifs } from "../../src/controllers/FetchGifs.js";
 import mockHttpResponse from "../utils/__mock__/mockHttpResponse.json";
-import Constants from "../../src/constants/AppConstants.js";
+import { expectedTruncatedGifData_0, expectedTruncatedGifData_1 } from "./helpers.js";
 
 jest.mock("axios");
+axios.get.mockImplementation(() => Promise.resolve(mockHttpResponse));
 
 it("should pass correct params to axios", async () => {
-  axios.get.mockImplementationOnce(() => Promise.resolve(mockHttpResponse));
   await fetchGifs(Constants.GIPHY_TRENDING_ENDPOINT, Constants.API_KEY, 0, Constants.LIMIT_GIFS_PER_LOAD);
   expect(axios.get)
     .toBeCalledWith(
       Constants.GIPHY_TRENDING_ENDPOINT,
       { "params": { "api_key": Constants.API_KEY, "limit": Constants.LIMIT_GIFS_PER_LOAD, "offset": 0 } }
     );
+});
+
+it("should fetch truncated gif data", async () => {
+  const { truncatedGifs, pagination } = await fetchGifs(
+    Constants.GIPHY_TRENDING_ENDPOINT, Constants.API_KEY, 0, Constants.LIMIT_GIFS_PER_LOAD);
+
+  expect(pagination).toEqual({ total_count: 111561, count: 2, offset: 0 });
+  expect(truncatedGifs).toHaveLength(2);
+  expect(truncatedGifs[0]).toEqual(expectedTruncatedGifData_0);
+  expect(truncatedGifs[1]).toEqual(expectedTruncatedGifData_1);
 });
---
+++ test/controlers/helpers.js
@@ -0,0 +1,67 @@
+export const expectedTruncatedGifData_0 = {
+  id: 'KD8Ldwzx90X9hi9QHW',
+  title: 'Tired At Home GIF by IKEA USA',
+  gif: {
+    fixed_height: {
+      "height": "200",
+      "mp4": "https://media1.giphy.com/media/KD8Ldwzx90X9hi9QHW/200.mp4?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=200.mp4",
+      "mp4_size": "64286",
+      "size": "594120",
+      "url": "https://media1.giphy.com/media/KD8Ldwzx90X9hi9QHW/200.gif?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=200.gif",
+      "webp": "https://media1.giphy.com/media/KD8Ldwzx90X9hi9QHW/200.webp?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=200.webp",
+      "webp_size": "170404",
+      "width": "200",
+    },
+    original: {
+      "frames": "45",
+      "hash": "49799c0503173d5686fb238b34c9b55e",
+      "height": "480",
+      "mp4": "https://media1.giphy.com/media/KD8Ldwzx90X9hi9QHW/giphy.mp4?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=giphy.mp4",
+      "mp4_size": "320065",
+      "size": "4363372",
+      "url": "https://media1.giphy.com/media/KD8Ldwzx90X9hi9QHW/giphy.gif?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=giphy.gif",
+      "webp": "https://media1.giphy.com/media/KD8Ldwzx90X9hi9QHW/giphy.webp?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=giphy.webp",
+      "webp_size": "796904",
+      "width": "480",
+    }
+  },
+  user: {
+    avatar_url: 'https://media4.giphy.com/avatars/IKEAUSA/cSMrv32MRdWa.png',
+    username: 'IKEAUSA',
+    profile_url: 'https://giphy.com/IKEAUSA/'
+  }
+};
+
+export const expectedTruncatedGifData_1 = {
+  id: 'WoifpFfPMrbQG875JC',
+  title: 'Wake Up Sleeping GIF by The Creamlovers',
+  gif: {
+    fixed_height: {
+      "height": "200",
+      "mp4": "https://media4.giphy.com/media/WoifpFfPMrbQG875JC/200.mp4?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=200.mp4",
+      "mp4_size": "14446",
+      "size": "13352",
+      "url": "https://media4.giphy.com/media/WoifpFfPMrbQG875JC/200.gif?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=200.gif",
+      "webp": "https://media4.giphy.com/media/WoifpFfPMrbQG875JC/200.webp?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=200.webp",
+      "webp_size": "11490",
+      "width": "267"
+    },
+    original: {
+      "frames": "5",
+      "hash": "b8414dcb8401d840775d7b62ed0bbfa7",
+      "height": "480",
+      "mp4": "https://media4.giphy.com/media/WoifpFfPMrbQG875JC/giphy.mp4?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=giphy.mp4",
+      "mp4_size": "30694",
+      "size": "32117",
+      "url": "https://media4.giphy.com/media/WoifpFfPMrbQG875JC/giphy.gif?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=giphy.gif",
+      "webp": "https://media4.giphy.com/media/WoifpFfPMrbQG875JC/giphy.webp?cid=1405890bvvaqtyx2ngrgzlcnsrztqed883xibfpiksp32ywj&rid=giphy.webp",
+      "webp_size": "33530",
+      "width": "640"
+    }
+  },
+  user: {
+    avatar_url: 'https://media0.giphy.com/avatars/Creamlovers/5NOufBa97ZA6.gif',
+    username: 'Creamlovers',
+    profile_url: 'https://giphy.com/Creamlovers/'
+  }
+};
